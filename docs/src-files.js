0f3cb24f218d760bb20cc598d466d275a065ff4e
Iavor S. Diatchki <diatchki@galois.com>
2025-12-21 12:31:10 -0800

    Docs

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["nn",["",[],["common.rs","lib.rs","norm.rs"]]],["nn",["",[],["common.rs","lib.rs","output.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["nn",["",[],["agent.rs","common.rs","lib.rs","norm.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[47,50]}
+//{"start":36,"fragment_lengths":[58]}
--- docs/trait.impl/core/marker/trait.Copy.js
+++ docs/trait.impl/core/marker/trait.Copy.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["nn",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"nn/struct.Dim.html\" title=\"struct nn::Dim\">Dim</a>"]]]]);
+    var implementors = Object.fromEntries([["nn",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"nn/norm/struct.ISigmoid.html\" title=\"struct nn::norm::ISigmoid\">ISigmoid</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"nn/norm/struct.OBitVec.html\" title=\"struct nn::norm::OBitVec\">OBitVec</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"nn/norm/struct.OClassifier.html\" title=\"struct nn::norm::OClassifier\">OClassifier</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"nn/norm/struct.ORVec.html\" title=\"struct nn::norm::ORVec\">ORVec</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"nn/struct.Dim.html\" title=\"struct nn::Dim\">Dim</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[235]}
+//{"start":57,"fragment_lengths":[1244]}
